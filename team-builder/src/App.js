a4becc88b5abfb2b16fb594f07e38e0fc1751906
Marissa <marissacooterl@gmail.com>
2020-03-24 17:14:02 -0500

    finished last step, refactoring for DRY

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,19 +1,29 @@
-import React, {useState} from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Member from './member';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Member from "./member";
+import Form from "./Form";
 
 function App() {
   const [teamMembers, setTeamMembers] = useState([
-    {firstName: 'marissa', lastName: 'cooter', role: 'nonFurry'},
-    {firstName:'zoe', lastName:'stokes', role:'furry'}
-]);
+    { name: "marissa", email: "nonFurry@gmail.com", role: "nonFurry" },
+    { name: "zoe", email: "furry@gmail.com", role: "furry" }
+  ]);
+
+  const addMem = (name, email, role) => {
+    // exisiting = existing piece of state
+    setTeamMembers(existing => [...existing, {name: name, email: email, role: role}])
+  } 
+
   return (
-  <div>
-    {teamMembers.map(member => (
-      <Member teamMember={member}/>
-    ))}
-  </div>
+    <>
+      <div>
+        {teamMembers.map(member => (
+          <Member teamMember={member} />
+        ))}
+      </div>
+      <Form addMem = {addMem}/>
+    </>
   );
 }
 
---
+++ team-builder/src/Form.js
@@ -0,0 +1,53 @@
+import React, {useState} from "react";
+
+const Form = (props) => {
+  
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [role, setRole] = useState('');
+
+  const changeName = (event) => {
+    setName(event.target.value)
+  }
+  const changeEmail = (event) => {
+    setEmail(event.target.value)
+  }
+  const changeRole = (event) => {
+    setRole(event.target.value)
+  }
+
+  const submitForm = (event, name, email, role) => {
+    event.preventDefault()
+    props.addMem(name, email, role)
+    setName('')
+    setEmail('')
+    setRole('')
+  }
+  
+  return (
+    <div>
+      <form onSubmit = {event => submitForm(event, name, email, role)}>
+        <label>
+          Name:{" "}
+          <input
+            id="firstName"
+            type="text"
+            onChange= {event => changeName(event)}
+            value={name}
+          />
+        </label>
+
+        <label>
+          Email: <input id="lastName" type="text" onChange={event => changeEmail(event)} value={email} />
+        </label>
+
+        <label>
+          Role: <input id="role" type="text" onChange={event => changeRole(event)} value={role} />
+        </label>
+        <input type='submit'/>
+      </form>
+    </div>
+  );
+};
+
+export default Form;
